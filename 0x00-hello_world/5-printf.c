a8a431f523675aa9dd368d125f62839e934f8926
mosherif33 <m.sherif2311@gmail.com>
2023-06-19 18:11:00 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printif function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printif("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
