bfd4e3732d81d46f14ac369e6a7be00665ef2830
mosherif33 <m.sherif2311@gmail.com>
2023-06-26 19:39:22 -0700

    a

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates a random valid password for 101-crackme.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+    int i;
+    char password[12];
+
+    srand(time(NULL));
+
+    for (i = 0; i < 11; i++)
+    {
+        password[i] = rand() % 94 + 33;
+    }
+
+    password[11] = '\0';
+
+    printf("%s", password);
+
+    return (0);
+}
