d48a871a1e2e56758298398b86cf2191450895b7
mosherif33 <m.sherif2311@gmail.com>
2023-06-30 19:40:31 -0700

    a

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: The string to be capitalized
+ *
+ * Return: Pointer to the capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int capitalize_next = 1;
+
+	while (str[i] != '\0')
+	{
+		if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 'a' + 'A'; /* Convert lowercase to uppercase */
+		}
+
+		capitalize_next = 0;
+
+		switch (str[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '\"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				capitalize_next = 1;
+				break;
+			default:
+				break;
+		}
+
+		i++;
+	}
+
+	return str;
+}
