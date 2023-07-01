92e6204bc5aa197e225cfd8bea7eea1b2ffb62b6
mosherif33 <m.sherif2311@gmail.com>
2023-06-30 19:52:44 -0700

    a

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,45 +1,93 @@
-[200~#include "main.h"
+#include "main.h"
+
 /**
- *  * infinite_add - adds two numbers
- *   * @n1: first number
- *    * @n2: second number
- *     * @r: buffer for result
- *      * @size_r: buffer size
- *       *
- *        * Return: address of r or 0
- *         */
+ * get_size - get the string size
+ * @s: the string
+ *
+ * Return: the size of the string
+ */
+int get_size(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * get_val - get the value of the digit
+ * @s: the string
+ * @index: the index of the digit
+ *
+ * Return: integer value
+ */
+int get_val(char *s, int index)
+{
+	int val;
+
+	if (index < 0)
+	{
+		val = 0;
+	}
+	else
+	{
+		val = s[index] - '0';
+	}
+	return (val);
+}
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: the first number
+ * @n2: the second number
+ * @r: the buffer used to store the result
+ * @size_r: the buffer size
+ *
+ * Return: a pointer to the result
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
-		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	int size_n1, size_n2, max_size, i, n1_val, n2_val, carry;
+
+	size_n1 = get_size(n1);
+	size_n2 = get_size(n2);
+	max_size = (size_n1 > size_n2) ? (size_n1) : (size_n2);
+	if ((max_size + 1) > size_r)
 	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+		return (0);
 	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
+	i = max_size;
+	while (i > 0)
 	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		n1_val = get_val(n1, size_n1);
+		n2_val = get_val(n2, size_n2);
+		r[i] = (n1_val + n2_val + carry) % 10 + '0';
+		carry = (n1_val + n2_val + carry) / 10;
+
+		i--;
+		size_n1--;
+		size_n2--;
+	}
+	r[max_size + 1] = '\0';
+	if (carry > 0)
+	{
+		if (max_size + 2 < size_r)
+		{
+			i = max_size;
+			while (i >= 0)
+			{
+				r[i + 1] = r[i];
+				i--;
+			}
+		}
+		else
+		{
+			return (0);
+		}
+		r[0] = carry + '0';
 	}
 	return (r);
 }
