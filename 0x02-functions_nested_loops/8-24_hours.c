40543ee2fd351f55a12fb146b0a3fdefeecaaddc
mosherif33 <m.sherif2311@gmail.com>
2023-07-02 20:12:48 -0700

    a

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * jack_bauer - Print every minute of the day
+ * Description: prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int t = 0;
+	int a = 0, b = 0, c = 0, d = 0;
+
+	while (t < 1440)
+	{
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+
+		d++;
+		if (d > 9)
+		{
+			d = 0;
+			c++;
+		}
+		if (c > 5)
+		{
+			c = 0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		t++;
+	}
+}
