1678d9261a027e28cfb0781df46873668936f748
mosherif33 <m.sherif2311@gmail.com>
2023-07-04 06:41:04 -0700

    a

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,14 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 #include <stdio.h>
-void set_string(char **s, char *to);
-void print_diagsums(int *a, int size);
-void print_chessboard(char (*a)[8]);
-char *_strstr(char *haystack, char *needle);
-char *_strpbrk(char *s, char *accept);
-unsigned int _strspn(char *s, char *accept);
-char *_memcpy(char *dest, char *src, unsigned int n);
+#include <stdlib.h>
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-#endif /* MAIN_H */
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+#endif/*MAIN_H*/
